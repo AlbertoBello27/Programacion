6ba6a61da3b01d6abe1c55e6119332bb6158d422
Alberto Bello <AlbertoBello27@users.noreply.github.com>
2023-04-06 15:34:05 -0400

    Mi primer commit

---
+++ VariablesGit.py
@@ -0,0 +1,45 @@
+"""UN SIMPLE PRINT"""
+print ("Hola")
+
+#UN SIMPLE PRINT 
+print ("ola") 
+print ("pan con palta")
+print ("-")
+
+#DECLARO VARIABLE
+name = "Alberto"
+nombre = "Ignacio"
+x = "Bello"
+z= "Soto"
+print (name)
+print (nombre)
+print (x)
+print (z)
+print("-")
+print ("Hola soy", name)
+print ("Hola soy", nombre)
+print ("Hola soy",name,nombre,x,z)
+s = ", me gusta la palta"
+print ("Hola soy",name,nombre,x,z,s)
+
+#OTRA VARIABLE
+edad = 19
+print ("Hola tengo",edad)
+
+#MAS COSAS
+print ("Hola soy",name)
+print ("Hola soy"+name)
+
+#print ("hola mi nombre es"+name,nombre,x,z+"y tengo"+edad)
+print ("Hola mi nombre es"+name+nombre+x+z+"y tengo"+str(edad))
+print ("Hola mi nombre es",name,nombre,x,z,"y tengo",str(edad))
+print ("Hola mi nombre es"+" name ")
+print ("Hola mi nombre es")
+
+#VARIABLE MUTABLE
+name = "Pablo"
+print ("Hola mi nuevo nombre es",name)
+
+#OTRA INSTRUCCION
+nombre = input("¿Cual es tu nombre?")
+print("tu nombre es",name,"mentiroso")
