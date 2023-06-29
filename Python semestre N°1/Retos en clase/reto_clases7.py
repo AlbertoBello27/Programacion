90689c8aaedcd7ab08f148c13ec892e5c7776c95
AlbertoBello27 <129216858+AlbertoBello27@users.noreply.github.com>
2023-06-28 23:45:47 -0400

    Delete Python semestre N°1 directory

--- Python semestre N°1/Retos en clase/reto_clases7.py
+++
@@ -1,9 +0,0 @@
-def ejercicio8():
-    separada=frase.split()
-    for i in separada:
-        diccionario[i]=len(i)
-    print(diccionario)
-diccionario={}
-frase=input("Ingrese una frase: ")
-
-ejercicio8()
