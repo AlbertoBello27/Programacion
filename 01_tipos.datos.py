158746d351936f0ed44f8a9e16564c3ad923822d
AlbertoBello27 <129216858+AlbertoBello27@users.noreply.github.com>
2023-06-28 23:30:57 -0400

    Delete 01_tipos.datos.py

--- 01_tipos.datos.py
+++
@@ -1,9 +0,0 @@
-print("Hola mundo")
-print("si")
-print("adios")
-nombre = input("¿Cual es tu nombre?\n")
-edad = input("¿Que edad tienes?\n")
-total_edad = int(edad) + 20
-formato = f"hola tu nombre es {nombre}, tienes {str(edad)} años y en 20 años tendras {str(total_edad)} años)"
-print(formato)
-print("prueba")
